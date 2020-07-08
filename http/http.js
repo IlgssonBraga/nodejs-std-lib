b599614827e6a0a92c7d7feeb91f91278a08a4f4
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-08 20:46:12 -0300

    feat: http server

--- http/http.js
+++ http/http.js
@@ -1,3 +1,15 @@
 const http = require("http");
 
-const app = http.createServer();
+http
+  .createServer((req, res) => {
+    const url = req.url;
+
+    if (url === "/") {
+      res.write("Hello");
+      res.end();
+    } else if (url === "/about") {
+      res.write(JSON.stringify({ name: "Ilgsson" }));
+      res.end();
+    }
+  })
+  .listen(3333, () => console.log("Server running on http://localhost:3333"));
